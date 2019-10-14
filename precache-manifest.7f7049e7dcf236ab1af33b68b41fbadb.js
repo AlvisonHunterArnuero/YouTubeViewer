9a9dc9ccac3b7fff499157d54e784b3fe308078b
Alvison Hunter <alvisonhunter@Jimmys-MacBook-Pro.local>
2019-10-14 15:58:59 -0600

    Updates

--- precache-manifest.7f7049e7dcf236ab1af33b68b41fbadb.js
+++ precache-manifest.7f7049e7dcf236ab1af33b68b41fbadb.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "1e56b791b29e9093fdfc3e86d30994d6",
+    "revision": "a5036f14900274d752f39e19f31c4fb0",
     "url": "/YouTubeViewer/index.html"
   },
   {
-    "revision": "52507e54903dd3b9e0e1",
+    "revision": "1dd3276f0738ea4b442b",
     "url": "/YouTubeViewer/static/css/main.6c18dba5.chunk.css"
   },
   {
@@ -12,8 +12,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/YouTubeViewer/static/js/2.a6606e75.chunk.js"
   },
   {
-    "revision": "52507e54903dd3b9e0e1",
-    "url": "/YouTubeViewer/static/js/main.9ff04a9f.chunk.js"
+    "revision": "1dd3276f0738ea4b442b",
+    "url": "/YouTubeViewer/static/js/main.7bf87997.chunk.js"
   },
   {
     "revision": "88059199fd60c42e5b96",
