a70bea4018e3e0e976eb97a7fde0c28b1ba16bf0
Alvison Hunter <alvisonhunter@Jimmys-MacBook-Pro.local>
2019-10-14 15:54:56 -0600

    build added

---
+++ docs/precache-manifest.bbac5aa2652334575e3a986d2c579a54.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e56b791b29e9093fdfc3e86d30994d6",
+    "url": "/YouTubeViewer/index.html"
+  },
+  {
+    "revision": "52507e54903dd3b9e0e1",
+    "url": "/YouTubeViewer/static/css/main.6c18dba5.chunk.css"
+  },
+  {
+    "revision": "9f55c5038f14592e97d6",
+    "url": "/YouTubeViewer/static/js/2.a6606e75.chunk.js"
+  },
+  {
+    "revision": "52507e54903dd3b9e0e1",
+    "url": "/YouTubeViewer/static/js/main.9ff04a9f.chunk.js"
+  },
+  {
+    "revision": "88059199fd60c42e5b96",
+    "url": "/YouTubeViewer/static/js/runtime-main.17b134f1.js"
+  }
+]);
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.85951188d7bb5cfd95b83b1647de0eae.js"
+  "/YouTubeViewer/precache-manifest.bbac5aa2652334575e3a986d2c579a54.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/YouTubeViewer/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
