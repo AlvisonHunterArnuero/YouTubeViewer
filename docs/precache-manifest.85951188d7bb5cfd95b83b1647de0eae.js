b4cfb7493d58dfe7661d8699a4c9e63318fbdb85
Alvison Hunter <alvisonhunter@Jimmys-MacBook-Pro.local>
2019-10-14 15:47:33 -0600

    docs added

---
+++ docs/precache-manifest.85951188d7bb5cfd95b83b1647de0eae.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df6c4d843f862756731e3fb54831e522",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0fd5e0e6c4f1735cd0c7",
+    "url": "/static/css/main.6c18dba5.chunk.css"
+  },
+  {
+    "revision": "9f55c5038f14592e97d6",
+    "url": "/static/js/2.a6606e75.chunk.js"
+  },
+  {
+    "revision": "0fd5e0e6c4f1735cd0c7",
+    "url": "/static/js/main.75e791b0.chunk.js"
+  },
+  {
+    "revision": "1082f238a8f27278947d",
+    "url": "/static/js/runtime-main.7f690eb3.js"
+  }
+]);
